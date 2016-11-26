20f0d1b8890cab4cd146480fbf52d3ff2d69c8f5
gater <gater@gater-PC>
2016-11-26 17:26:23 +0300

    newvar

--- NewProj/src/de/javateam/main.java
+++ NewProj/src/de/javateam/main.java
@@ -3,7 +3,8 @@ package de.javateam;
 public class main {
 
 	public static void main(String[] args) {
-	  int a = 5;
+	  int d = 7;
+	  int j=7;
 		//TODO Auto-generated method stub
 	System.out.println();
 		
