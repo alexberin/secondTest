1f322cac5bb09d2e47455688636d62d5d50fed0c
alexberin <berinalex86@gmail.com>
2016-11-26 15:34:17 +0100

    Some changes on variables.

--- NewProj/src/de/javateam/main.java
+++ NewProj/src/de/javateam/main.java
@@ -4,7 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 	  int d = 7;
-	  int j=7;
+	  int j = 7;
+	  int x = 10;
 		//TODO Auto-generated method stub
 	System.out.println();
 		
